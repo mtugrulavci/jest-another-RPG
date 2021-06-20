2878d01f4c27cde962914b29793f7bc5698d1b83
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-06-20 14:16:35 -0400

    prototype functions added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,7 +3,7 @@ jest.mock('../lib/Potion');//it mocks the actual function
 console.log(new Potion());
 
 const Player = require('../lib/Player');
-
+// test 1
 test('creates a player object', () =>{
 const player = new Player('Dave');
 
@@ -15,5 +15,23 @@ expect(player.agility).toEqual(expect.any(Number));
 expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
-
 });
+//test 2
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+//test 3
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
